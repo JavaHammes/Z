d737422fba42197e12f3e43934602399427f1e2c
Alexander Berlin <alexander.berlin13@gmail.com>
2024-12-04 14:55:59 +0100

    Introduction of a debuggee struct. This will be neccessary for handling specific actions on the debuggee more easily. For example: - run(dbgee); - stop(dbgee); - continue(dbgee); etc.

--- src/main.c
+++ src/main.c
@@ -12,18 +12,14 @@ int main(int argc, char **argv) {
                 FATAL("Usage: %s <debug_target>\n", argv[0]);
         }
 
-        const char *target_name = argv[1];
+        const char *debuggee_name = argv[1];
 
-        if (!file_exists(target_name)) {
-                FATAL("Cannot find executable %s", target_name);
+        if (!file_exists(debuggee_name)) {
+                FATAL("Cannot find executable %s", debuggee_name);
         }
 
-        debugger dbg = {
-            .target_pid = -1,
-            .target_name = target_name,
-            .debugger_state_flag = DEBUGGER_IDLE,
-            .target_state_flag = TARGET_IDLE,
-        };
+        debugger dbg;
+        init_dbg(&dbg, debuggee_name);
 
         if (start_dbg(&dbg) != 0) {
                 free_dbg(&dbg);
