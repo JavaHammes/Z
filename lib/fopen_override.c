ea3bcfda3f67b36192949e9815e141c77d0a271a
JavaHammes <85684605+JavaHammes@users.noreply.github.com>
2025-02-01 15:17:21 +0100

    Hook prctl and Improve timing_analysis Consistency (#50)

--- lib/fopen_override.c
+++ lib/fopen_override.c
@@ -7,32 +7,37 @@
 typedef FILE *(*orig_fopen_f_type)(const char *, const char *);
 
 FILE *fopen(const char *__restrict__filename, const char *__modes) {
-    static orig_fopen_f_type real_fopen = NULL;
+        static orig_fopen_f_type real_fopen = NULL;
 
-    if (!real_fopen) {
-        void *handle = dlsym(RTLD_NEXT, "fopen");
-        if (!handle) {
-            (void)(fprintf(stderr, "Error in `dlsym`: %s\n", dlerror()));
-            return NULL;
+        if (!real_fopen) {
+                void *handle = dlsym(RTLD_NEXT, "fopen");
+                if (!handle) {
+                        (void)(fprintf(stderr, "Error in `dlsym`: %s\n",
+                                       dlerror()));
+                        return NULL;
+                }
+
+                union {
+                        void *ptr;
+                        orig_fopen_f_type func;
+                } cast;
+
+                cast.ptr = handle;
+                real_fopen = cast.func;
         }
 
-        union {
-            void *ptr;
-            orig_fopen_f_type func;
-        } cast;
+        (void)(fprintf(stderr,
+                       "[HOOK] fopen called with file=\"%s\", mode=\"%s\"\n",
+                       __restrict__filename, __modes));
 
-        cast.ptr = handle;
-        real_fopen = cast.func;
-    }
-
-    if (strstr(__restrict__filename, "/proc/self/status")) {
-        FILE *fake_file = tmpfile();
-        if (fake_file) {
-            (void)(fprintf(fake_file, "TracerPid: 0\n"));
-            rewind(fake_file);
-            return fake_file;
+        if (strstr(__restrict__filename, "/proc/self/status")) {
+                FILE *fake_file = tmpfile();
+                if (fake_file) {
+                        (void)(fprintf(fake_file, "TracerPid: 0\n"));
+                        rewind(fake_file);
+                        return fake_file;
+                }
         }
-    }
 
-    return real_fopen(__restrict__filename, __modes);
+        return real_fopen(__restrict__filename, __modes);
 }
